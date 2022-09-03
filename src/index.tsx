ef121be6c20abcbbf0f94916aa55b1abea1a7a77
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-09-02 22:53:29 -0700

    changed to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 
-
-
-render(
+const root = document.getElementById('root');
+createRoot(root!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
+
+// render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
